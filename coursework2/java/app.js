becadd44cb8e7bc215807fde6bbbdba379d6a8ec
Carolina Roques <carolina.roques@gmail.com>
2021-03-12 14:57:41 +0000

    .

--- coursework2/java/app.js
+++ coursework2/java/app.js
@@ -1,12 +1,12 @@
 openMenu = () => {
-  document.getElementsById("menu").style.right = '0';
-  document.getElementsById("cover").style.right = '0';
+  document.getElementById("menu").style.right = '0';
+  document.getElementById("cover").style.right = '0';
 }
 
 closeMenu = () => {
-  document.getElementsById("menu").style.right = '-100%';
-  document.getElementsById("cover").style.right = '-100%';
+  document.getElementById("menu").style.right = '-100%';
+  document.getElementById("cover").style.right = '-100%';
 }
 
   document.getElementsByClassName("burger").addEventListener("click",openMenu);
-  document.getElementsById("cover").addEventListener("click",closeMenu);
+  document.getElementById("cover").addEventListener("click",closeMenu);
